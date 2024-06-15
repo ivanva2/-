57f7824559936b73b270d3083e2791716fce190c
ivanva2 <vasiliev.ivan2003@yandex.ru>
2024-06-15 22:31:21 +0300

    izmen

--- prog.py
+++ prog.py
@@ -59,7 +59,7 @@ class Dictionary:
         self.tree.heading('#0', text = 'id', anchor = CENTER)
         self.tree.heading('#1', text = 'Вид матерьяла', anchor = CENTER)
         ttk.Button(text = 'Удалить', command = self.delete_word).grid(row = 5, column = 0, sticky = W + E)
-        ttk.Button(text = 'Изменить',).grid(row = 5, column = 1, sticky = W + E)
+        ttk.Button(text = 'Изменить',command=self.edit_word).grid(row = 5, column = 1, sticky = W + E)
         self.get_words()
         
    
@@ -113,6 +113,46 @@ class Dictionary:
         query = f'DELETE FROM typesofmaterials WHERE id = {typeofmaterial}'
         self.run_query(query,False, )
         self.message['text'] = 'Слово {} успешно удалено'.format(typeofmaterial)
+        self.get_words()
+    # рeдактирование слова и/или значения
+    def edit_word(self):
+        self.message['text'] = ''
+        try:
+            self.tree.item(self.tree.selection())['values'][0]
+        except IndexError as e:
+            self.message['text'] = 'Выберите строку для изменения'
+            return
+        word = self.tree.item(self.tree.selection())['text']
+        old_meaning = self.tree.item(self.tree.selection())['values'][0]
+        self.edit_wind = Toplevel()
+        self.edit_wind.title = 'Изменить значение'
+
+        Label(self.edit_wind, text = 'Прежнее id:').grid(row = 0, column = 1)
+        Entry(self.edit_wind, textvariable = StringVar(self.edit_wind, value = word), state = 'readonly').grid(row = 0, column = 2)
+        
+        Label(self.edit_wind, text = 'Новое id:').grid(row = 1, column = 1)
+        # предзаполнение поля
+        new_word = Entry(self.edit_wind, textvariable = StringVar(self.edit_wind, value = word))
+        new_word.grid(row = 1, column = 2)
+
+
+        Label(self.edit_wind, text = 'Прежнее вид матерьяла:').grid(row = 2, column = 1)
+        Entry(self.edit_wind, textvariable = StringVar(self.edit_wind, value = old_meaning), state = 'readonly').grid(row = 2, column = 2)
+ 
+        Label(self.edit_wind, text = 'Новое вид матерьяла:').grid(row = 3, column = 1)
+        # предзаполнение поля
+        new_meaning= Entry(self.edit_wind, textvariable = StringVar(self.edit_wind, value = old_meaning))
+        new_meaning.grid(row = 3, column = 2)
+
+        Button(self.edit_wind, text = 'Изменить', command = lambda: self.edit_records(new_word.get(), word, new_meaning.get(), old_meaning)).grid(row = 4, column = 2, sticky = W)
+        self.edit_wind.mainloop()
+    # внесение изменений в базу
+    def edit_records(self, new_word, word, new_meaning, old_meaning):
+        query = f"""UPDATE typesofmaterials SET id = {new_word}, typeofmaterial = '{new_meaning}'WHERE id = {word} AND typeofmaterial = '{old_meaning}'"""
+        
+        self.run_query(query,False)
+        self.edit_wind.destroy()
+        self.message['text'] = 'слово {} успешно изменено'.format(word)
         self.get_words()
     
     
